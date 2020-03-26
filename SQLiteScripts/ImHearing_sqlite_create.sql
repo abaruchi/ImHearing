698ecb3e853af2783e1d33efe5e87df143f41ba3
Artur Baruchi <artur.baruchi@gmail.com>
2020-03-26 16:23:28 -0300

    Updates SQL Script removing the Schedule Table

--- SQLiteScripts/ImHearing_sqlite_create.sql
+++ SQLiteScripts/ImHearing_sqlite_create.sql
@@ -1,10 +1,10 @@
 CREATE TABLE RecordTable (
-	record_id            varchar(255) NOT NULL  PRIMARY KEY  ,
+	record_id            varchar(255) NOT NULL    ,
 	record_date          date     ,
 	record_start         datetime     ,
 	record_end           datetime     ,
 	record_size          bigint     ,
-	uploaded             boolean     ,
+	uploaded             varchar(100)     ,
 	path                 varchar(255)
  );
 
@@ -13,28 +13,17 @@ CREATE INDEX Idx_record_start ON RecordTable ( record_start );
 CREATE INDEX Idx_record_date ON RecordTable ( record_date );
 
 CREATE TABLE UserTable (
-	user_id              bigint NOT NULL  PRIMARY KEY  ,
+	user_id              bigint NOT NULL    ,
 	user_name            varchar(100) NOT NULL    ,
 	user_email           varchar(100)     ,
 	user_pass            varchar(100) NOT NULL    ,
-	user_lastlogin       datetime     ,
-	CONSTRAINT Idx_user_id UNIQUE ( user_id )
+	user_lastlogin       datetime
  );
 
-CREATE TABLE ScheduleRecordsTable (
-	schedule_id          bigint NOT NULL  PRIMARY KEY  ,
-	fk_user_id           bigint     ,
-	fk_record_id         varchar(255)     ,
-	schedule_start       datetime     ,
-	schedule_end         datetime     ,
-	FOREIGN KEY ( fk_user_id ) REFERENCES UserTable( user_id )  ,
-	FOREIGN KEY ( fk_record_id ) REFERENCES RecordTable( record_id )
- );
-
-CREATE INDEX Idx_schedule_start ON ScheduleRecordsTable ( schedule_start );
+CREATE INDEX Idx_user_id ON UserTable ( user_id );
 
 CREATE TABLE TokenTable (
-	token_id             varchar(255) NOT NULL  PRIMARY KEY  ,
+	token_id             varchar(255) NOT NULL    ,
 	fk_user_id           bigint     ,
 	token_expiration     datetime NOT NULL    ,
 	FOREIGN KEY ( fk_user_id ) REFERENCES UserTable( user_id )
