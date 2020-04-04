e887c2305335163d2de7ab3723bfaa7c7b16c7e6
Artur Baruchi <artur.baruchi@gmail.com>
2020-04-03 21:57:27 -0300

    Updates classes to be more efficient

--- main/audio.py
+++ main/audio.py
@@ -2,10 +2,11 @@
 Classes to Record, Compress, Store and Upload the Audios recorded.
 """
 
-import time
 import wave
+import zipfile
 from datetime import datetime
-from os import getcwd, stat
+from os import getcwd, stat, path, makedirs
+from uuid import uuid4
 
 import pyaudio
 
@@ -15,12 +16,11 @@ class Recorder(object):
     channels = 1
     sample_rate = 44100
 
-    def __init__(self, timestamp=None):
+    def __init__(self):
         # Values that can be changed
         self.record_time = 600
         self.chunk = 512
         self.device_index = 2
-        self.timestamp = int(time.time()) if timestamp is None else timestamp
         self.records_file_dict = dict()
 
     def record_start(self):
@@ -29,13 +29,16 @@ class Recorder(object):
         :return: Returns the file path with the recorded data.
         """
         audio = pyaudio.PyAudio()
-        frames = []
+        frames = list()
         stream = audio.open(format=Recorder.form_1,
                             rate=Recorder.sample_rate,
                             channels=Recorder.channels,
                             input_device_index=self.device_index,
                             input=True, frames_per_buffer=self.chunk)
-
+        record_uuid = uuid4()
+        record_file = self.__file_output(
+            file_name_parts=[str(record_uuid)[35 - 11:],
+                             str(int(datetime.now().timestamp()))])
         # Start the Recording
         time_start = datetime.now()
         for _ in range(0, (self.sample_rate // self.chunk) * self.record_time):
@@ -48,7 +51,7 @@ class Recorder(object):
         time_end = datetime.now()
         # Finish the Recording
 
-        input_file = self.__file_output()
+        input_file = record_file
         wavefile = wave.open(input_file, 'wb')
         wavefile.setnchannels(Recorder.channels)
         wavefile.setsampwidth(audio.get_sample_size(Recorder.form_1))
@@ -56,25 +59,32 @@ class Recorder(object):
         wavefile.writeframes(b''.join(frames))
         wavefile.close()
 
-        self.records_file_dict[str(datetime.now())] = {
+        self.records_file_dict[str(record_uuid)] = {
             'time_start': time_start,
             'time_end': time_end,
-            'file_path': input_file,
+            'file_path': record_file,
+            'zip_archive': None,
+            'registered': False,
+            'compressed': False,
             'uploaded': False,
-            'file_size': stat(input_file).st_size/(1024*1024)
+            'file_size': stat(record_file).st_size / (1024 * 1024)
         }
 
         return input_file
 
-    def __file_output(self, base_path=None):
-        output_file = 'record_' + str(self.timestamp) + '.wav'
+    def __file_output(self, base_path=None, file_name_parts=None):
+        file_partial = ''
+        if file_name_parts is not None:
+            for part in file_name_parts:
+                file_partial = file_partial + '_' + part
+        output_file = 'record_' + file_partial + '.wav'
         output_path = getcwd() if base_path is None else base_path
         return output_path + '/' + output_file
 
-    def recorded_files(self):
+    def get_metadata_dict(self):
         return self.records_file_dict
 
-    #-- Setters and Getters --#
+    # -- Setters and Getters --#
     @property
     def record_time(self):
         return self.__record_time
