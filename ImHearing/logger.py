e464a78553ae66f585f4eb496b0699ecbcef5235
Artur Baruchi <artur.baruchi@gmail.com>
2020-06-16 21:14:10 -0300

    Implements routines to create and handle log handlers

---
+++ ImHearing/logger.py
@@ -0,0 +1,46 @@
+""" File with routines to implements Logging
+"""
+
+import logging
+import sys
+from logging.handlers import TimedRotatingFileHandler
+
+FORMATTER = logging.Formatter(
+    "%(asctime)s — %(name)s — %(levelname)s — %(message)s")
+
+
+def get_console_handler():
+    """
+    Sends the Log to the Std Output set to the process
+    :return:
+    """
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(FORMATTER)
+    return console_handler
+
+
+def get_file_handler(log_file):
+    """
+    Sets a file handler to a given file
+    :param log_file: Path to be used as log
+    :return:
+    """
+    file_handler = TimedRotatingFileHandler(log_file, when='midnight')
+    file_handler.setFormatter(FORMATTER)
+    return file_handler
+
+
+def get_logger(logger_name, log_file):
+    """
+    Gets a logger to be used to a specific part of the system
+    :param logger_name: The name of the looger
+    :param log_file: The path to the log file
+    :return:
+    """
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(get_console_handler())
+    logger.addHandler(get_file_handler(log_file))
+
+    logger.propagate = False
+    return logger
