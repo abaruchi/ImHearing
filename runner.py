cbf653fa266a3754a16b175b2031f7b29829c6e2
Artur Baruchi <artur.baruchi@gmail.com>
2020-06-19 13:24:21 -0300

    Implements main loop to call cleanup, archive and upload routines

--- runner.py
+++ runner.py
@@ -5,7 +5,7 @@ from signal import SIGINT, signal
 
 from pony.orm.dbapiprovider import DatabaseError
 
-from ImHearing import audio, logger, reader
+from ImHearing import audio, logger, post_recording, pre_recording, reader
 from ImHearing.database import models
 
 # Configurations Sections
@@ -44,22 +44,46 @@ def exit_handler(signal_received, frame):
     my_logger = logger.get_logger("exit_handler", GLOBAL_CONFIG['log_file'])
     my_logger.info("Terminating - Reveived Signal {}".format(signal_received))
 
-    # --> Archive Routine Here
+    # Archive
+    post_recording.archive_records(db, GLOBAL_CONFIG)
+
     # --> Clean Up Routine Here
+    post_recording.remove_uploaded_records(db, GLOBAL_CONFIG)
+    post_recording.remove_uploaded_archives(db, GLOBAL_CONFIG)
     exit(0)
 
 
 def main():
 
-    # 1: Check if all conditions to record is set
-    #  1.1: Check Budget (if true stop the process)
-    #  1.1: Check file system usage OR number of records (archive and send to
-    #                                                      AWS)
-    # 2. Start recording
+    main_logger = logger.get_logger("runner", GLOBAL_CONFIG['log_file'])
 
-    ## Testing
-    for i in range(0, 3):
-        audio.start_recording(db, GLOBAL_CONFIG)
+    if pre_recording.check_aws_budget(db, AWS_CONFIG) < 0:
+        main_logger.error(
+            " -- AWS Budget {} exceeded -- ".format(AWS_CONFIG['budget_cost']))
+        exit(-1)
+
+    while True:
+        perform_cleanup_routines = False
+        if pre_recording.check_fs_usage(db, GLOBAL_CONFIG) < 0:
+            main_logger.error(
+                " -- FS Usage Check, Exceeded {}% -- ".format(
+                    GLOBAL_CONFIG['storage_usage']))
+            perform_cleanup_routines = True
+
+        if pre_recording.check_record_count(db, GLOBAL_CONFIG) < 0:
+            main_logger.error(
+                " -- Record Count Check, Exceeded {} --".format(
+                    GLOBAL_CONFIG['records_count']))
+            perform_cleanup_routines = True
+
+        if perform_cleanup_routines:
+            # Archive, Upload & Remove Records & Archives
+            post_recording.archive_records(db, GLOBAL_CONFIG)
+            post_recording.upload_archive(db, AWS_CONFIG, GLOBAL_CONFIG)
+            post_recording.remove_uploaded_archives(db, GLOBAL_CONFIG)
+            post_recording.remove_uploaded_records(db, GLOBAL_CONFIG)
+        else:
+            audio.start_recording(db, GLOBAL_CONFIG)
 
 
 if __name__ == '__main__':
